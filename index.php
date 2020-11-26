d5d0473651579cba34ff13baf9fa37da13ad92b8
yuliaxxx <32487219+yuliaxxx@users.noreply.github.com>
2020-11-26 17:18:25 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,1 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-	<title><?php echo "Задание OSINT"; ?></title>
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<meta content="text/html; charset=utf-8">
-</head>
-<body>
+<h1> Baldanovaa</h1>
